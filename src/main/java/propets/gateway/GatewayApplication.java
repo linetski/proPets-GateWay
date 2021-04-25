54133f814a826e75564a751bf4e5d6cb1a54d57c
Constantine <linetski@gmail.com>
2021-04-25 23:47:46 +0300

    revert to newer version

--- src/main/java/propets/gateway/GatewayApplication.java
+++ src/main/java/propets/gateway/GatewayApplication.java
@@ -1,24 +1,16 @@
 package propets.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
 @EnableZuulProxy
+@SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
-  }
-}
 
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
