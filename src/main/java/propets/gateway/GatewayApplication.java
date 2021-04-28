fb078ae78320f8144bb7ff1bc3b23bf89aaf3402
Constantine <linetski@gmail.com>
2021-04-29 01:00:13 +0300

    remove corsFilter

--- src/main/java/propets/gateway/GatewayApplication.java
+++ src/main/java/propets/gateway/GatewayApplication.java
@@ -18,21 +18,4 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 	
-	@Bean
-	public CorsFilter corsFilter() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.addAllowedOrigin("*http://localhost:3000");
-	    config.addAllowedHeader("Authorization");
-	    config.addAllowedMethod("OPTIONS");
-	    config.addAllowedMethod("HEAD");
-	    config.addAllowedMethod("GET");
-	    config.addAllowedMethod("PUT");
-	    config.addAllowedMethod("POST");
-	    config.addAllowedMethod("DELETE");
-	    config.addAllowedMethod("PATCH");
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
 }
