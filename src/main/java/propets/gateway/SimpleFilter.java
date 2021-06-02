93216575f5aa293c71911ca5edf13151feed8eaa
Constantine <linetski@gmail.com>
2021-06-02 16:20:13 +0300

    fix

--- src/main/java/propets/gateway/SimpleFilter.java
+++ src/main/java/propets/gateway/SimpleFilter.java
@@ -3,6 +3,8 @@ package propets.gateway;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 import com.netflix.zuul.context.RequestContext;
@@ -14,6 +16,10 @@ import org.slf4j.LoggerFactory;
 public class SimpleFilter extends ZuulFilter {
 
 	  private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+	  private static Date lastUpdated;
+	  static{
+		  lastUpdated = new Date();
+	  }
 
 	  @Override
 	  public String filterType() {
@@ -34,12 +40,21 @@ public class SimpleFilter extends ZuulFilter {
 	  public Object run() {
 	    RequestContext ctx = RequestContext.getCurrentContext();
 	    HttpServletRequest request = ctx.getRequest();
-	    triggerUrl("https://propets-eurekaservice.herokuapp.com");
-	    triggerUrl("https://propets-lostandfoundservice.herokuapp.com");
-	    triggerUrl("https://propets-auth-service.herokuapp.com");
-	    triggerUrl("https://propets-configuration-service.herokuapp.com");
-	    triggerUrl("https://propets-elastic-service.herokuapp.com");
 	    
+	    Date date = new Date();
+	    long diff = date.getTime() - lastUpdated.getTime();
+	    long minutes = TimeUnit.MILLISECONDS.toMinutes(diff); 
+	    if(minutes > 25) {
+	    	log.info("wakeup services start");
+		    triggerUrl("https://propets-eurekaservice.herokuapp.com");
+		    triggerUrl("https://propets-configuration-service.herokuapp.com");
+		    triggerUrl("https://propets-auth-service.herokuapp.com");
+		    triggerUrl("https://propets-lostandfoundservice.herokuapp.com");
+		    triggerUrl("https://propets-elastic-service.herokuapp.com");
+		    triggerUrl("https://propets-notification-service.herokuapp.com");
+		    log.info("wakeup services end");
+	    }
+	    lastUpdated = date;
 	    log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
 	    return null;
@@ -51,14 +66,11 @@ public class SimpleFilter extends ZuulFilter {
 				    URL url = new URL(urlString);
 				 // Open a connection(?) on the URL(??) and cast the response(???)
 				    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				    
-				 // Now it's "open", we can set the request method, headers etc.
-				    connection.setRequestProperty("accept", "application/json");
 
 				    // This line makes the request
-				    InputStream responseStream = connection.getInputStream();
+				    connection.getInputStream();
 				    } catch(Exception e) {
-				    	e.printStackTrace();
+				    	
 				    }
 	  }
 
